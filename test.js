7e8c4f5caed119db954661145562b76f80c06ad9
JongbeenSong <1041489@gmail.com>
2024-05-04 11:56:23 +0900

    to HMS vocal

--- test.js
+++ test.js
@@ -8,8 +8,6 @@ const checkAuthority = require("./src/util/check-authority.js");
 
 require("dotenv").config({ path: ".env" })
 
-let streamerName = "금성경";    // 테스트중
-
 buzzk.login(process.env.NID_AUT, process.env.NID_SES); //로그인
 
 const buzzkChat = buzzk.chat;
@@ -23,6 +21,7 @@ async function test (streamerName) {
     let channel = chSearch[0]; //검색 결과 첫번째 채널
 
     const lvDetail = await buzzk.live.getDetail(channel.channelID); //현재 방송 정보
+    logger.info(lvDetail);
 
     let chat = new buzzkChat(channel.channelID);
     await chat.connect(); //채팅창 연결
@@ -51,7 +50,7 @@ async function test (streamerName) {
             }
 
             if (isActive) {
-                if (streamerName === "금성경") {
+                if (streamerName === "일급천재") {
                     runCelestial(data, o, chat);
                 }
 
@@ -64,11 +63,16 @@ async function test (streamerName) {
             let userInfo = await chat.getUserInfo(data[o].author.id);
         	logger.info(userInfo); //채팅 보낸 유저의 정보
         }
+
+        chat.onDisconnect(async () => {
+            logger.info("방송과 연결이 끊겨 일싹이가 잠듭니다");
+            return;
+        })
     });
 }
 
-test("금성경");
+// test("금성경");
 // test("일급천재");
 // test("캐피탈호");
 // test("병겜임");
-// test("해모수보컬");
+test("해모수보컬");
